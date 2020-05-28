cc1161f77ac124f9e45f9669607797b68c4cbe22
Artem <woopmensch@ip-192-168-0-134.is74.loc>
2020-05-28 18:34:27 +0500

    test

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 4000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, '../client', 'dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     })
 }
