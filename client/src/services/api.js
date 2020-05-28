1bbebfbd37ea496ccd48a2b2d81bceca88e12089
Artem <woopmensch@ip-192-168-0-134.is74.loc>
2020-05-28 13:03:41 +0500

    test

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const HTTP = axios.create({
-    baseURL: '',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
