3d39006cef47e58044875648470db18009a332a7
Artem <woopmensch@ip-192-168-0-134.is74.loc>
2020-05-28 18:15:05 +0500

    test

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const HTTP = axios.create({
     baseURL: 'https://mevn-social-network.herokuapp.com/api',
     headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
     }
